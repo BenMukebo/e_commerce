8fd063f1840bc5d83624efd0941a2af3120565b7
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-06-24 08:20:43 +0200

    Add price to the mock products array and the product props

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Grid from '@material-ui/core';
 
 const products = [
-  {id: 1, name: 'shoes', description: 'Runnind shoes.'},
-  {id: 2, name: 'Macbook', description: 'Apple mackbook.'},
+  {id: 1, name: 'shoes', description: 'Runnind shoes.', price: '$5'},
+  {id: 2, name: 'Macbook', description: 'Apple mackbook.', price: '$10'},
 ];
 
 const Products = () => {
@@ -12,7 +12,7 @@ const Products = () => {
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
-            <Product />
+            <Product product={product} />
           </Grid>
         ))}
       </Grid>
