d504101288ab46a16a4d679cc9682ebd46fd5e5c
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-06-26 07:40:51 +0200

    Import styles.js product in Product component  and Products component into App.js

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 
-
+import useStyles from './styles';
 
 const Product = ({ product }) => {
+
+  const classes = useStyles();
 
   return (
     <Card className={classes.root}>
       <CardMedia className={classes.media} image='' title={product.name} />
+
       <CardContent>
         <div className={classes.cardContent}>
           <Typography gutterBottom variant="h5">
@@ -22,8 +25,9 @@ const Product = ({ product }) => {
           { product.description }
         </Typography>
       </CardContent>
+
       <CardActions disableSpacing className={classes.cardActions}>
-        <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
+        <IconButton aria-label="Add to Cart">
           <AddShoppingCart />
         </IconButton>
       </CardActions>
