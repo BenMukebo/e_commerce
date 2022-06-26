e8a5e783d67d5165ee6c777f1a73aa5564fa888f
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-06-26 08:25:01 +0200

    Add images to the mock array

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,8 +4,8 @@ import {  Grid } from '@material-ui/core';
 import Product from './Product/Product';
 
 const products = [
-  {id: 1, name: 'shoes', description: 'Runnind shoes.', price: '$5'},
-  {id: 2, name: 'Macbook', description: 'Apple mackbook.', price: '$10'},
+  {id: 1, name: 'shoes', description: 'Runnind shoes.', price: '$5', image: 'https://user-images.githubusercontent.com/79058364/175802212-ae743832-4c70-4cd2-b7a3-81428102b3e5.jpeg'},
+  {id: 2, name: 'Macbook', description: 'Apple mackbook.', price: '$10', image: 'https://user-images.githubusercontent.com/79058364/175802292-f3d12888-f296-4ca2-a7f2-7e689d6d7385.jpeg'},
 ];
 
 const Products = () => {
