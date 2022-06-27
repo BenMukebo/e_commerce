f030835333de53ef99d1b3a0de1ba5ad8c31446a
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-06-27 06:40:57 +0200

    Make the products useState with an emply array as default value

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { Navbar, Products } from './components';
 import { commerce } from './lib/commerce';
 
 const App = () => {
+
+  const [products, setProducts] = useState([]);
+
+
+
   return (
     <div>
       <Navbar/>
