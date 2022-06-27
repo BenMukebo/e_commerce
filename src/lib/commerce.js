5c59a4540aecf7e0f0e9e4a74c86c614c1108f27
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-06-27 05:20:45 +0200

    Create lib folder directory that going to be our store

---
+++ src/lib/commerce.js
@@ -0,0 +1,6 @@
+import Commerce from '@chec/commerce.js';
+
+export const commerce = new Commerce();
+
+// We're basically creating a new instance of that specificique commerce and that's going to be our store
+// we need to put as first parametre inside Commwece() our public key from the store 
