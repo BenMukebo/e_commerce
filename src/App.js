4534111e6cc6624f75e1e3463139794b6da51bb5
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-06-29 16:35:53 +0200

    Fix react-router bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import { Navbar, Products, Cart } from './components';
 import { commerce } from './lib/commerce';
@@ -34,19 +37,13 @@ const App = () => {
   // console.log(cart);
 
   return (
-    <Router>
-      <div>
-        <Navbar totalItems={cart.total_items} />
-        <Switch>
-          <Route exact path="/">
-            <Products products={products} onAddToCart={handleAddToCart} />
-          </Route>
-          <Route exact path="/cart">
-            <Cart cart={cart} />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      <Navbar totalItems={cart.total_items} />
+      <Routes>
+        <Route exact path="/" element={<Products products={products} onAddToCart={handleAddToCart} />} />
+        <Route exact path="/cart" element={<Cart cart={cart} />} />
+      </Routes>
+    </div>
   );
 };
 
