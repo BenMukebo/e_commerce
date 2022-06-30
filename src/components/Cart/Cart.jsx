bfcdfbbc9cf2305780638d555e04ce7a7fcc80a6
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-06-30 12:06:10 +0200

    Pass onUpdateCartQty and onRemoveCart methods as props to the CartItem component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,7 +8,9 @@ import { Link } from 'react-router-dom';
 import CartItem from './CartItem/CartItem';
 import useStyles from './styles';
 
-const Cart = ({ cart }) => {
+const Cart = ({
+  cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart,
+}) => {
   const classes = useStyles();
   // const isEmpty = !cart.line_items?.length;
   // console.log(cart.line_items?.length); ?: just mean wait for the data load
@@ -28,7 +30,12 @@ const Cart = ({ cart }) => {
         {cart.line_items.map((lineItem) => (
           <Grid item xs={12} sm={4} key={lineItem.id}>
             {/* <div>{lineItem.name}</div> */}
-            <CartItem item={lineItem} />
+            <CartItem
+              item={lineItem}
+              onUpdateCartQty={onUpdateCartQty}
+              onRemoveFromCart={onRemoveFromCart}
+            />
+
           </Grid>
         ))}
       </Grid>
@@ -39,7 +46,7 @@ const Cart = ({ cart }) => {
           {cart.subtotal.formatted_with_symbol}
         </Typography>
         <div>
-          <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty cart</Button>
+          <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" >Empty cart</Button>
           <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
         </div>
       </div>
--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -7,7 +7,7 @@ import {
 
 import useStyles from './styles';
 
-const CartItem = ({ item }) => {
+const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart }) => {
   const classes = useStyles();
 
   return (
